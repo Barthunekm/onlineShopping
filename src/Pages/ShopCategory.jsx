72a3bf27eea29a32a3e5c712b72b06eb80f2f0db
Barthunekm <kavita.m.barthune@gmail.com>
2024-01-05 15:25:48 +0530

    loginsignupadd

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -10,10 +10,10 @@ const {all_product} =useContext(ShopContext)
 
     return (
         <div className='shop-category'>
-            <img src={props.banner} alt="" />
+            <img className='shopcategory-banner' src={props.banner} alt="" />
             <div className="shopcategory-indexSort">
                 <p>
-                    <span>showing 1-12</span>out of 36 products
+                    <span>showing 1-12</span> out of 36 products
                 </p>
                 <div className="shopcategory-sort">
                     Sort by <img src={dropdown_icon} alt="" />
@@ -22,16 +22,18 @@ const {all_product} =useContext(ShopContext)
             <div className="shopcategory-products">
                 {all_product.map((item,i)=>{
                   if(props.category===item.category){
-                        return <Item key={i} id ={item.id} name={item.name
-                        } image={item.image} new_price={item.new_price} old_price={item.old_price}/>    
+                        return <Item key={i} id ={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>    
                   }
-                  else{
+                   else{
                     return null;
                   }
 
                 })}
                  
             </div>
+          <div className="shopcategory-loadmore">
+             Explore More
+            </div>  
         </div>
 
     );
